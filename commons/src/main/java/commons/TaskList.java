36d7abc18ad7681ca8f77ad1f2390ad94ef4e6c1
Max Eradus <m.t.j.eradus@student.tudelft.nl>
2023-03-07 15:11:34 +0100

    Created the database and added anotations in the classed for spring boot

--- commons/src/main/java/commons/TaskList.java
+++ commons/src/main/java/commons/TaskList.java
@@ -4,18 +4,22 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
 @Entity
+@Table(name = "TaskList")
 public class TaskList {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "task")
     @OneToMany
     private List<Task> task;
 
