7b961e57108b243da680d7ff6581a0be1863efe6
AlexisFimeyer <a.h.p.a.fimeyer@student.tudelft.nl>
2023-03-08 14:20:41 +0100

    Added the get mapping for boards so it returns the board and its tasklist

--- server/src/main/java/server/api/BoardController.java
+++ server/src/main/java/server/api/BoardController.java
@@ -2,10 +2,14 @@ package server.api;
 
 
 import commons.Board;
+import commons.Task;
+import commons.TaskList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.BoardRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/boards")
@@ -15,22 +19,17 @@ public class BoardController {
     private BoardRepository boardRepository;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Board> getBoardById(@PathVariable Long id){
+    public ResponseEntity<BoardWithTaskListDto> getBoardById(@PathVariable Long id){
         Board board = boardRepository.findById(id).orElse(null);
         if (board == null){
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(board);
+        List<TaskList> taskList = board.getTasklist();
+        BoardWithTaskListDto dto = new BoardWithTaskListDto(board, taskList);
+        return ResponseEntity.ok(dto);
     }
 
-    @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteBoardById(@PathVariable Long id){
-        Board board = boardRepository.findById(id).orElse(null);
-        if (board == null){
-            return ResponseEntity.notFound().build();
-        }
-        boardRepository.delete(board);
-        return ResponseEntity.noContent().build();
-    }
+
+
 
 }
