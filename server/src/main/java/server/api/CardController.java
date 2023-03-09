441c59c06c7da52077e0bfa195742b2a867d497e
Ewout de Dobbelaar <e.w.dedobbelaar@student.tudelft.nl>
2023-03-09 19:16:15 +0100

    7-created an endpoint for deleting cards

--- server/src/main/java/server/api/CardController.java
+++ server/src/main/java/server/api/CardController.java
@@ -2,13 +2,19 @@ package server.api;
 
 import commons.Task;
 import commons.TaskList;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 import server.database.TaskListRepository;
 import server.database.TaskRepository;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/boards/list/task")
 public class CardController {
 
     private TaskListRepository taskListRepository;
@@ -19,15 +25,48 @@ public class CardController {
         this.taskRepository = taskRepository;
     }
 
-    @PostMapping(path = "/boards/list/add-card")
-    public ResponseEntity<Task> add(@RequestBody Task task, @RequestBody TaskList taskList){
+    @GetMapping(path = {"/test"})
+    public String oi() {
+        return "oi";
+    }
+
+    @PostMapping(path = "/add-card")
+    public ResponseEntity<Task> add(@RequestBody Task task){        // , @RequestBody TaskList taskList
 
         if (task.getName() == null || task.getDescription() == null) {
             return ResponseEntity.badRequest().build();
         }
 
-        taskList.add(task);
+        //taskList.add(task);
+        taskRepository.save(task);
+        // TODO: tasklist adding
 
         return ResponseEntity.ok(task);
     }
+
+    @GetMapping("/get-all-cards")
+    public ResponseEntity<List<Task>> get() {
+        return ResponseEntity.ok(taskRepository.findAll());
+    }
+
+    @DeleteMapping("/delete-card")
+    public ResponseEntity<Object> deleteTask(@RequestBody Task task) {
+        if (task == null || isNullOrEmpty(task.getDescription()) || isNullOrEmpty(task.getName()) || task.getId() == null) return ResponseEntity.badRequest().build();
+        if (!taskRepository.exists(Example.of(task))) return ResponseEntity.badRequest().build();
+        //return ResponseEntity.ok(taskRepository.getById(task.getId()));
+        if (!task.equals(taskRepository.findAll(Example.of(task)).get(0))) return ResponseEntity.badRequest().build();
+        taskRepository.delete(task);
+        // TODO: delete the entry in the taskList
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Checks if a sting is null or empty
+     * @param s     The Sting to be checked
+     * @return      Boolean
+     */
+    private static boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+
 }
