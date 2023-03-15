86ccfec8da77e0391339ca027e9c60471017f937
Samuel <s.e.vandenhouten@student.tudelft.nl>
2023-03-16 00:59:46 +0100

    Improve style

--- client/src/main/java/client/components/BoardComponent.java
+++ client/src/main/java/client/components/BoardComponent.java
@@ -3,6 +3,7 @@ package client.components;
 import commons.Board;
 import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 
 
@@ -10,9 +11,10 @@ public class BoardComponent extends AnchorPane {
     public BoardComponent(Board board) {
         super();
         Node[] taskLists = board.getTasklist().stream().map(TaskListComponent::new).toArray(Node[]::new);
-        TilePane taskListContainer = new TilePane(taskLists);
+        HBox taskListContainer = new HBox(taskLists);
+        taskListContainer.setSpacing(45.0);
         AnchorPane.setTopAnchor(taskListContainer, 150.0);
         AnchorPane.setLeftAnchor(taskListContainer, 150.0);
-        this.getChildren().add(taskListContainer);
+        getChildren().add(taskListContainer);
     }
 }
--- client/src/main/java/client/components/TaskComponent.java
+++ client/src/main/java/client/components/TaskComponent.java
@@ -1,13 +1,19 @@
 package client.components;
 
 import commons.Task;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class TaskComponent extends VBox {
+    private static final String style = "-fx-background-color: #f7f7f5; -fx-border-width: 2; -fx-border-color: gray;  -fx-border-radius: 10 10 10 10;-fx-background-radius: 10 10 10 10;";
     public TaskComponent(Task task) {
         super();
         Label title = new Label(task.getName());
-        this.getChildren().add(title);
+        getChildren().add(title);
+        setMaxSize(200, 80);
+        setMinSize(200, 80);
+        setStyle(style);
+        setAlignment(Pos.CENTER);
     }
 }
