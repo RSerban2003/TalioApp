002249c7c1df48bfc0a8d0e4e040c7c097887ea4
AlexisFimeyer <a.h.p.a.fimeyer@student.tudelft.nl>
2023-03-16 01:11:01 +0100

    When clicking retrieve board, if the board exists, it will be querried and the result will be put into a board object, which can then be used to be displayed

--- client/src/main/java/client/scenes/BoardInputCtrl.java
+++ client/src/main/java/client/scenes/BoardInputCtrl.java
@@ -10,6 +10,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardInputCtrl {
     @FXML
     private TextField boardIdTextField;
