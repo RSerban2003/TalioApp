c3f6003584e7272d28a72ed402db1314aabf9800
Samuel van den Houten <s.e.vandenhouten@student.tudelft.nl>
2023-03-20 12:13:37 +0100

    Add error handling to edit list endpoint

--- server/src/main/java/server/api/TaskListController.java
+++ server/src/main/java/server/api/TaskListController.java
@@ -3,6 +3,7 @@ package server.api;
 
 import commons.Board;
 import commons.TaskList;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.BoardRepository;
@@ -36,8 +37,13 @@ public class TaskListController {
         return ResponseEntity.ok(taskList);
     }
     @PostMapping(path = "/{list}/edit")
-    public ResponseEntity<TaskList> edit(@PathVariable("board") long boardId, @PathVariable("list") long listId, @RequestParam String name) throws RuntimeException {
-        TaskList taskList = taskListRepository.findById(listId).orElseThrow(() -> new RuntimeException("List not found"));
+    public ResponseEntity<?> edit(@PathVariable("board") long boardId, @PathVariable("list") long listId, @RequestParam String name) throws RuntimeException {
+        if(!boardRepository.existsById(boardId)) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Board not found");
+
+        if(!taskListRepository.existsById(listId)) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("List not found");
+        TaskList taskList = taskListRepository.findById(listId).get();
+
+        if(taskList.getBoard().getId() != boardId) return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("List not part of board");
         taskList.setName(name);
         taskListRepository.save(taskList);
         return ResponseEntity.ok(taskList);
