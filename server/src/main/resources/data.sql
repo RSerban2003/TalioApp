5e45f24afab14f23fa8384dd3aab4c2d23ad8f21
Max Eradus <m.t.j.eradus@student.tudelft.nl>
2023-03-21 19:17:33 +0100

    added springboot websocket and starter web dependenties

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,7 +1,6 @@
-DELETE FROM task WHERE id=100;
-DELETE FROM task WHERE id=101;
-DELETE FROM tasklist WHERE id=100;
-DELETE FROM board WHERE id=100;
+DELETE FROM task;
+DELETE FROM tasklist;
+DELETE FROM board;
 INSERT INTO board (id, title) VALUES (100, 'blabla');
 INSERT INTO Tasklist (id, name, board_id) VALUES (100, 'aaaa', 100);
 INSERT INTO Tasklist (id, name, board_id) VALUES (101, 'aaaa', 100);
