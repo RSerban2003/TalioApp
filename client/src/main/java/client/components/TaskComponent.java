6d72ab8cedcea2752ad53437b500f759fe3c9346
Samuel <s.e.vandenhouten@student.tudelft.nl>
2023-03-26 01:22:54 +0100

    feat: make task draggable

--- client/src/main/java/client/components/TaskComponent.java
+++ client/src/main/java/client/components/TaskComponent.java
@@ -1,12 +1,16 @@
 package client.components;
 
 import commons.Task;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class TaskComponent extends VBox {
     private static final String style = "-fx-background-color: #f7f7f5; -fx-border-width: 2; -fx-border-color: gray;  -fx-border-radius: 10 10 10 10;-fx-background-radius: 10 10 10 10;";
+    private long taskId;
     public TaskComponent(Task task) {
         super();
         Label title = new Label(task.getName());
@@ -15,5 +19,9 @@ public class TaskComponent extends VBox {
         setMinSize(200, 80);
         setStyle(style);
         setAlignment(Pos.CENTER);
+        this.taskId = task.getId();
+    }
+    public long getTaskId() {
+        return taskId;
     }
 }
--- client/src/main/java/client/components/TaskListComponent.java
+++ client/src/main/java/client/components/TaskListComponent.java
@@ -1,17 +1,33 @@
 package client.components;
 
 import commons.TaskList;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 
 public class TaskListComponent extends VBox {
     private static final String style = "-fx-background-color: #c7c7c7; -fx-border-width: 2; -fx-border-color: gray; -fx-font-weight: bold; -fx-border-radius: 10 10 10 10; -fx-background-radius: 10 10 10 10;";
     public TaskListComponent(TaskList taskList) {
         super();
-        Node[] tasks = taskList.getTaskList().stream().map(TaskComponent::new).toArray(Node[]::new);
+        TaskComponent[] tasks = taskList.getTaskList().stream().map(TaskComponent::new).toArray(TaskComponent[]::new);
+        for (TaskComponent task: tasks) {
+            task.setOnDragDetected(new EventHandler<MouseEvent>() {
+                public void handle(MouseEvent event) {
+                    Dragboard db = task.startDragAndDrop(TransferMode.ANY);
+                    ClipboardContent content = new ClipboardContent();
+                    content.putString(Long.toString(task.getTaskId()));
+                    db.setContent(content);
+                    event.consume();
+                }
+            });
+        }
         getChildren().add(new Label(taskList.getName()));
         getChildren().addAll(tasks);
         setStyle(style);
