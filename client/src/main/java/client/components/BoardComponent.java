7f1c6de666e5b386e3bcacd19690552178ccce3b
Rares Serban <R.Serban@student.tudelft.nl>
2023-03-27 17:12:04 +0200

    fix: removed some code that interfered with the displaying of taskLists

--- client/src/main/java/client/components/BoardComponent.java
+++ client/src/main/java/client/components/BoardComponent.java
@@ -29,7 +29,7 @@ public class BoardComponent extends AnchorPane {
     public void update(Board board, ServerUtils server) {
         Platform.runLater(
             () -> {
-                Node[] taskLists = board.getListOfTaskList().stream().map((TaskList taskList) -> {TaskListComponent taskListComponent = new TaskListComponent(taskList, board, server); taskListComponent.addTask(mainCtrl); return taskListComponent;}).toArray(Node[]::new);
+                Node[] taskLists = board.getListOfTaskList().stream().map((TaskList taskList) -> new TaskListComponent(taskList, board, server)).toArray(Node[]::new);
                 HBox taskListContainer = new HBox(taskLists);
                 taskListContainer.setSpacing(45.0);
                 AnchorPane.setTopAnchor(taskListContainer, 150.0);
