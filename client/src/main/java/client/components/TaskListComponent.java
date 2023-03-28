1e470039388dcfce8244e29b6153d4736ff3f295
Farhan Farhan Taufiqillah Lingga Djaya <farhantaufiqillahlinggadjaya@student.tudelft.nl>
2023-03-29 00:00:03 +0200

    Added front-end implementation of edit 'task list name' + set Vgrow of anchorPane to always + changed layout for whole task list a little bit

--- client/src/main/java/client/components/TaskListComponent.java
+++ client/src/main/java/client/components/TaskListComponent.java
@@ -4,10 +4,14 @@ import client.utils.ServerUtils;
 import commons.Board;
 import commons.Task;
 import commons.TaskList;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -33,33 +37,73 @@ public class TaskListComponent extends VBox {
             });
         }
 
-        // Create label for task list name
-        Label nameLabel = new Label(taskList.getName());
-        VBox nameBox = new VBox(nameLabel);
-        nameBox.setAlignment(Pos.TOP_CENTER);
-
-        // Create button
-        Button button = new Button("Delete");
         AnnotationConfigApplicationContext context
-            = new AnnotationConfigApplicationContext();
+                = new AnnotationConfigApplicationContext();
         context.scan("client");
         context.refresh();
         ServerUtils server = context.getBean(ServerUtils.class);
-        button.setOnAction(event -> server.deleteTaskList(board.getId(), taskList.getId()));
-        HBox buttonBox = new HBox(button);
-        buttonBox.setAlignment(Pos.TOP_RIGHT);
 
-        // Add nameBox and buttonBox to top row
-        HBox topRow = new HBox(nameBox, buttonBox);
-        topRow.setSpacing(20.0);
-        topRow.setAlignment(Pos.TOP_CENTER);
+        // Create label for task list name
+        Label nameLabel = new Label(taskList.getName());
+        nameLabel.setAlignment(Pos.CENTER);
 
-        // Add topRow and tasks to TaskListComponent
-        getChildren().add(topRow);
+        //VBox nameBox = new VBox(nameLabel);
+        //nameBox.setAlignment(Pos.TOP_CENTER);
+
+        // Create Edit button to edit label
+        Button editButton = new Button("Edit");
+        TextField nameField = new TextField();
+        nameField.setPromptText("e.g. School");
+        Button saveButton = new Button("Save");
+
+        // hide text field and save button at the start
+        nameField.setVisible(false);
+        nameField.setMaxWidth(200);
+        nameField.setAlignment(Pos.CENTER);
+        saveButton.setVisible(false);
+
+        // Create delete button
+        Button deleteButton = new Button("Delete");
+        deleteButton.setOnAction(event -> server.deleteTaskList(board.getId(), taskList.getId()));
+        //HBox buttonBox = new HBox(deleteButton);
+        //buttonBox.setAlignment(Pos.TOP_RIGHT);
+
+        // puts all top elements in a GridPane
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.TOP_CENTER);
+        gridPane.setHgap(10.0);
+        gridPane.setVgap(10.0);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        GridPane.setHalignment(nameLabel, HPos.CENTER);
+        gridPane.add(deleteButton, 0, 0);
+        gridPane.add(nameLabel, 1, 1);
+        gridPane.add(nameField, 1, 1);
+        gridPane.add(editButton, 2,1);
+        gridPane.add(saveButton, 2, 1);
+
+        // enables user to enter new label
+        editButton.setOnAction(e -> {
+            nameLabel.setVisible(false);
+            editButton.setVisible(false);
+            nameField.setVisible(true);
+            saveButton.setVisible(true);
+        });
+
+        saveButton.setOnAction(e ->{
+            if (!(nameField.getText().trim().isEmpty() || nameField.getText() == null)){
+                nameLabel.setText(nameField.getText());
+            }
+            nameLabel.setVisible(true);
+            editButton.setVisible(true);
+            nameField.setVisible(false);
+            saveButton.setVisible(false);
+        });
+
+        getChildren().add(gridPane);
         getChildren().addAll(tasks);
         setStyle(style);
         setAlignment(Pos.TOP_CENTER);
-        setPrefSize(270, 700);
+        setPrefSize(300, 700);
         setSpacing(20.0);
         this.taskList = taskList;
     }
