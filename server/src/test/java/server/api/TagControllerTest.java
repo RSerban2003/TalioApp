b171c840d2e4eef1b87e219faa6932a1086208e7
Rares Serban <R.Serban@student.tudelft.nl>
2023-04-07 15:03:56 +0200

    Fix: fixed pipeline

--- server/src/test/java/server/api/TagControllerTest.java
+++ server/src/test/java/server/api/TagControllerTest.java
@@ -11,7 +11,6 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import server.database.BoardRepository;
 import server.database.TagRepository;
 import server.database.TaskListRepository;
@@ -40,7 +39,7 @@ public class TagControllerTest {
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
